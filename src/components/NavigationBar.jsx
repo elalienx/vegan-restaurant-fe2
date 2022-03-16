6ec6929746bd15a6dd27730e72756af29460075c
Eduardo <el_alienx@hotmail.com>
2022-03-16 10:41:12 +0100

    read variables

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 export default function NavigationBar() {
   return (
     <nav>
-      <Link to="/">(👩‍🌾 Green farm bar & grill)</Link> ---
-      <Link to="menu/dishes">Dishes</Link> |
-      <Link to="menu/desert">Desert</Link> |
-      <Link to="menu/drinks">Drinks</Link> |
+      <Link to="/">(👩‍🌾 Green farm bar & grill)</Link>
+      <Link to="menu/dishes">Dishes</Link>
+      <Link to="menu/desert">Desert</Link>
+      <Link to="menu/drinks">Drinks</Link>
       <Link to="/contact">Contact</Link>
     </nav>
   );
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,7 +1,13 @@
+// NPM packages
+import { useParams } from "react-router-dom";
+
 export default function Category() {
+  const { category } = useParams();
+
   return (
     <div id="categoy">
       <h1>Category 🟢</h1>
+      <span>@{category}@</span>
     </div>
   );
 }
