7a741af52f9496a4bd1a4f89cd6a2effeed28f8e
Eduardo <el_alienx@hotmail.com>
2022-03-21 14:33:41 +0100

    moar stuff

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,20 @@
+// Project files
+import OwnerImage from "assets/images/restaurant-owner.jpg";
+
 export default function Contact() {
   return (
     <div id="contact">
-      <h1>Contact</h1>
+      <header className="hero">
+        <div className="centered-content">
+          <h1>Hi, Im Otto Smich</h1>
+          <span>Here is my vegan restaurant project</span>
+        </div>
+        <img src={OwnerImage} alt="A man with putting sugar on a lemon" />
+      </header>
+      <section className="container">
+        <h2>Contact us</h2>
+        <p>This section is comming soon...</p>
+      </section>
     </div>
   );
 }
